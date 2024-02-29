845ba7d84f2c08d020af15c7a3d1427e3f5e705b
Oleg1969691 <140894667+Oleg1969691@users.noreply.github.com>
2024-02-29 14:11:28 +0000

    Deploying to gh-pages from @ Oleg1969691/goit-js-hw-10@b48d5ec7368f59bae2a060287e29ea31a509c7ab 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-3c4e11fa.js";import{i as r}from"./assets/vendor-77e16229.js";const s=document.querySelector(".form");s.addEventListener("submit",o=>{o.preventDefault();const t=parseInt(s.elements.delay.value),i=s.elements.state.value;new Promise((e,m)=>{i==="fulfilled"?setTimeout(()=>{e(t)},t):i==="rejected"&&setTimeout(()=>{m(t)},t)}).then(e=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
